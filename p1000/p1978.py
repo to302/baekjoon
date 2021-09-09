0f1838f63c8dcccd90331482296954c932380b29
SangYoung Kwak <to302@nate.com>
2021-09-09 15:47:58 +0900

    소수- 보완

--- p1000/p1978.py
+++ p1000/p1978.py
@@ -11,14 +11,14 @@ N = int(input())
 Ns = list(map(int, input().split())) # 수는 1,000 이하의 자연수
 
 def prime_list(n):
-    sieve  = [True] * n
+    sieve  = [True] * (n+1)
     m = int(n ** 0.5)
     for i in range(2, m+1):
         if sieve[i] == True:
-            for j in range(i+i, n, i):
+            for j in range(i+i, n+1, i):
                 sieve[j] = False
 
-    return [i for i in range(2, n) if sieve[i] == True]
+    return [i for i in range(2, n+1) if sieve[i] == True]
 
 
 pl = prime_list(1000)
