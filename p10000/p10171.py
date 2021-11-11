639f93da37522fee4cb47980c2b44cfd7058aecc
SangYoung Kwak <to302@nate.com>
2021-11-11 13:42:48 +0900

    단순 print

---
+++ p10000/p10171.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+'''고양이
+https://www.acmicpc.net/problem/10171
+'''
+
+a = r'''\    /\
+ )  ( ')
+(  /  )
+ \(__)|'''
+
+print(a)
+
+# pass
---
+++ p10000/p10172.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+'''개
+https://www.acmicpc.net/problem/10172
+'''
+
+a = r'''|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|'''
+
+print(a)
