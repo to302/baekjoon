ca720db2f1f47f34ff374c738072df105dd86caa
SangYoung Kwak <to302@nate.com>
2021-11-11 09:42:06 +0900

    EOF 처리

---
+++ p11000/p11718.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+'''그대로 출력하기
+https://www.acmicpc.net/problem/11718
+'''
+
+while True:
+    try:
+        s = input()
+        print(s)
+    except EOFError:
+        break
+
+# pass
+
+# 구현
+# 문자열
+# EOF 처리
