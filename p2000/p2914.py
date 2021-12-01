6066a723563142cef137f9f39be5bb4311377448
SangYoung Kwak <to302@nate.com>
2021-12-02 08:58:08 +0900

    수학, 사칙연산, 구현

---
+++ p2000/p2914.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+'''저작권
+https://www.acmicpc.net/problem/2914
+'''
+
+A, I = map(int, input().split())
+print(A*(I-1) + 1)
+
+
+'''평균보다 1 작은값이면 올림이 안되는데, 올림을 위해서는 1작은 평균으로 구한 값보다 1단위 커야 한다.'''
+
+
+# pass
+
+# 수학
+# 구현
+# 사칙연산
