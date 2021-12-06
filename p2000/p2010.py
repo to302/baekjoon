33009abcb5fc882ee314b10862199da505b1cbd6
SangYoung Kwak <to302@nate.com>
2021-12-06 09:28:34 +0900

    수학, 사칙연산

---
+++ p2000/p2010.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+'''플러그
+https://www.acmicpc.net/problem/2010
+'''
+
+import sys
+input = sys.stdin.readline
+
+plug = 1
+for _ in range(int(input())):
+    i = int(input())
+    plug += (i-1)
+
+print(plug)
+
+
+# pass
+
+# 수학
+# 구현
+# 사칙연산
