0ab95f8d339c9e479af01ee26a7bec662096af68
SangYoung Kwak <to302@nate.com>
2021-12-08 10:41:00 +0900

    구현, 별찍기-13

---
+++ p2000/p2523.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+'''별 찍기 - 13
+https://www.acmicpc.net/problem/2523
+'''
+
+N = int(input())
+
+for i in range(1, N+1):
+    print("*" * i)
+
+for i in range(N-1, 0, -1):
+    print("*" * i)
+
+# pass
+
+# 구현
