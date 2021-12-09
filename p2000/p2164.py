4935ac3ee71450e54c10e998ce3022f194e37814
SangYoung Kwak <to302@nate.com>
2021-12-09 09:07:31 +0900

    개선

--- p2000/p2164.py
+++ p2000/p2164.py
@@ -9,18 +9,34 @@ from collections import deque
 N = int(input()) # 1 <= N <= 50,0000
 que = deque([i for i in range(1, N+1)])
 
-idx = 0
-while len(que) > 1:
-    if idx % 2 == 0:
+def fn1():
+    idx = 0
+    while len(que) > 1:
+        if idx % 2 == 0:
+            que.popleft()
+        else:
+            que.append(que.popleft())
+
+        idx += 1
+
+    print(que.popleft())
+
+
+def fn2():
+    while len(que) > 1:
         que.popleft()
-    else:
         que.append(que.popleft())
 
-    idx += 1
+    print(que.popleft())
 
-print(que.popleft())
 
-# pass
+
+if __name__ == '__main__':
+    fn1()
+    fn2() # faster
+
+
+# pass - fn1, fn2
 
 # 자료구조
 # 큐
