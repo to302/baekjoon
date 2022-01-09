774002620e606c095afe7ce1b230bb980934c132
Kwak SangYoung <to302@nate.com>
2022-01-09 22:40:30 +0900

    구현, 정렬

--- p2000/p2108.py
+++ p2000/p2108.py
@@ -1,32 +1,52 @@
 # coding=utf-8
 
-# https://www.acmicpc.net/problem/2108
-# 수를 처리하는 것은 통계학에서 상당히 중요한 일이다. 통계학에서 N개의 수를 대표하는 기본 통계값에는 다음과 같은 것들이 있다. 
-# 단, N은 홀수라고 가정하자.
-# 산술평균 : N개의 수들의 합을 N으로 나눈 값
-# 중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
-# 최빈값 : N개의 수들 중 가장 많이 나타나는 값
-# 범위 : N개의 수들 중 최댓값과 최솟값의 차이
-# N개의 수가 주어졌을 때, 네 가지 기본 통계값을 구하는 프로그램을 작성하시오.
-# 입력
-# 첫째 줄에 수의 개수 N(1 ≤ N ≤ 500,000)이 주어진다. 그 다음 N개의 줄에는 정수들이 주어진다. 입력되는 정수의 절댓값은 4,000을 넘지 않는다.
-# 출력
-# 첫째 줄에는 산술평균을 출력한다. 소수점 이하 첫째 자리에서 반올림한 값을 출력한다.
-# 둘째 줄에는 중앙값을 출력한다.
-# 셋째 줄에는 최빈값을 출력한다. 여러 개 있을 때에는 최빈값 중 두 번째로 작은 값을 출력한다.
-# 넷째 줄에는 범위를 출력한다.
+'''통계학
+https://www.acmicpc.net/problem/2108
 
-n = int(input())
-nl = []
-for _ in range(n):
+첫째 줄에 수의 개수 N(1 ≤ N ≤ 500,000)이 주어진다. 단, N은 홀수이다. 그 다음 N개의 줄에는 정수들이 주어진다. 입력되는 정수의 절댓값은 4,000을 넘지 않는다.
+'''
+
+import sys
+input = sys.stdin.readline
+
+
+N = int(input())
+dic = dict()
+for _ in range(N):
     i = int(input())
-    nl.append(i)
+    if i in dic.keys():
+        dic[i] += 1
+    else:
+        dic[i] = 1
 
-nl.sort()
-print(round(sum(nl)/n))
-print(nl[round(n/2)])
+items = dic.items()
 
 
+# 산술평균
+print(round(sum([k*v for k, v in items]) / N))
+    
+# 중앙값
+m = (N//2) + 1
+acc = 0
+for k, v in sorted(items):
+    acc += v
+    if acc >= m:
+        print(k)
+        break
+
+# 최빈값
+a = sorted(items, key=lambda x: (x[1],-x[0]), reverse=True)
+print(a[0][0] if (len(a)==1 or a[0][1] > a[1][1]) else a[1][0])
+
+# 범위
+a = max(items, key=lambda x:x[0]) 
+b = min(items, key=lambda x:x[0])
+print(a[0] - b[0])
 
 
-print(nl[-1] - nl[0])
+# pass -> from collections import Counter 을 이용하는 방법도 있음.(내 방식이 빠르긴 함)
+# statistics 모듈 이용하는 방법  multimode (3.8 에 추가됨)
+
+
+# 구현
+# 정렬
