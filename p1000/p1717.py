32380e90d288f443bd40db67e6cbdc4b591f0719
SangYoung Kwak <to302@nate.com>
2022-01-17 13:25:25 +0900

    시간초과

--- p1000/p1717.py
+++ p1000/p1717.py
@@ -8,27 +8,42 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-ns = [{i} for i in range(n+1)]
-nd = {i:i for i in range(n+1)}
+ns = [[i, None] for i in range(n+1)] # [parent, child]
+
+def get_root(idx):
+    global ns
+    while True:
+        if ns[idx][0] == idx:
+            return(idx)
+
+        idx = ns[idx][0]
+
+def get_leaf(idx):
+    global ns
+    while True:
+        if ns[idx][1] == None:
+            return(idx)
+
+        idx = ns[idx][1]
+
 
 for _ in range(m):
     c, a, b = map(int, input().split())
 
-    if c == 0:
+    if c == 0: # 합치기
         if a == b:
             continue
-        
-        i1 = nd[a] 
-        i2 = nd[b]
-        for i in ns[i2]:
-            nd[i] = nd[a]
-        ns[i1].update(ns[i2])
-        ns[i2] = {}
 
-    elif c == 1:
-        print('YES' if nd[a] == nd[b] else 'NO')
-    
-# 메모리 초과
+        la = get_leaf(a)
+        rb = get_root(b)
+        ns[la][1] = rb
+        ns[rb][0] = la
+        
+    elif c == 1: # 출력
+        print('YES' if a==b or get_root(a)==get_root(b) else 'NO')
+
+# 시간 초과
+
 
 # 자료 구조
 # 분리 집합    
