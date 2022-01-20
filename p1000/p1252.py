1ddaf4be1768aa40ba21da9cd2565a3ca8925cc3
SangYoung Kwak <to302@nate.com>
2022-01-20 16:37:08 +0900

    사칙연산, 이진수 덧셈

---
+++ p1000/p1252.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+'''이진수 덧셈
+https://www.acmicpc.net/problem/1252
+'''
+
+a, b = map(lambda x: int(x, base=2), input().split())
+print(bin(a + b)[2:])
+
+# pass
+
+# 수학
+# 구현
+# 사칙연산
