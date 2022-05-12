32d3804bcd1c2c550c83634daa19f9ad610dd2dd
SangYoung Kwak <to302@nate.com>
2022-05-12 16:20:55 +0900

    분리집합, disjoint set

--- p1000/p1717.py
+++ p1000/p1717.py
@@ -4,46 +4,42 @@
 https://www.acmicpc.net/problem/1717
 '''
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-ns = [[i, None] for i in range(n+1)] # [parent, child]
+parent = [i for i in range(n+1)]
 
-def get_root(idx):
-    global ns
-    while True:
-        if ns[idx][0] == idx:
-            return(idx)
+def get_root(x):
+    if x == parent[x]:
+        return x
+    else:
+        parent[x] = get_root(parent[x])
+        return parent[x] 
+    
+def union_root(x, y):
+    xr = get_root(x)
+    yr = get_root(y)
 
-        idx = ns[idx][0]
-
-def get_leaf(idx):
-    global ns
-    while True:
-        if ns[idx][1] == None:
-            return(idx)
-
-        idx = ns[idx][1]
-
+    if xr != yr:
+        parent[xr] = yr
+    
 
 for _ in range(m):
-    c, a, b = map(int, input().split())
-
-    if c == 0: # 합치기
-        if a == b:
-            continue
-
-        la = get_leaf(a)
-        rb = get_root(b)
-        ns[la][1] = rb
-        ns[rb][0] = la
+    op, a, b = map(int, input().split())
+    if op == 0:
+        union_root(a, b)
+    elif op == 1:
+        if get_root(a) == get_root(b):
+            print('YES')
+        else:
+            print('NO')
         
-    elif c == 1: # 출력
-        print('YES' if a==b or get_root(a)==get_root(b) else 'NO')
 
-# 시간 초과
-
+# pass
 
 # 자료 구조
-# 분리 집합    
+# 분리 집합 (Disjoint Set)
+#   
+# 참고 : https://4legs-study.tistory.com/94
